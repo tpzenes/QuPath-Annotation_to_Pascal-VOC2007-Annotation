6e4d67db6ad883bfc018e7c1c7fe163465ffe8a9
tpzenes <tpzenes@gmail.com>
2020-08-19 10:00:38 +0300

    Create export_annotations_qupath.groovy

---
+++ export_annotations_qupath.groovy
@@ -0,0 +1,57 @@
+import qupath.lib.objects.PathObjects
+
+import java.awt.image.BufferedImage;
+import java.io.File;
+import javax.imageio.ImageIO;
+
+def images = []
+
+def imgs_path = "/home/safir/Downloads/" 
+
+def project = getProject()
+
+
+
+for (entry in project.getImageList()) {
+
+    def hierarchy = entry.readHierarchy()
+
+    int index = 0
+    
+    
+
+    def annot = []
+    
+    for (annotation in hierarchy.getAnnotationObjects()) {
+
+        def roi = annotation.getROI()
+
+        def isaretleme = ['index': index, 'label': annotation.pathClass.name, 'leftTopx': roi.x, 'leftTopy': roi.y, 'xwidth': roi.x2 - roi.x, 'yheight': roi.y2 - roi.y]
+
+        annot << isaretleme
+
+        index = index + 1
+
+    }
+    
+    def img = ImageIO.read(new File("/" + imgs_path + "/" + entry));
+    
+    def server = getCurrentImageData().getServer()
+    
+    def tarama = ['Width':img.getWidth(),'Height':img.getHeight(),'filename': entry.getImageName(), 'isaretler': annot]
+
+    images << tarama
+
+}
+
+ 
+
+boolean prettyPrint = true
+
+def gson = GsonTools.getInstance(prettyPrint)
+
+FileWriter writer = new FileWriter("/home/safir/Desktop/QuPath_Test/test.txt")
+
+gson.toJson(images,writer)
+
+writer.close()
